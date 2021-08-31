bfd959a93fc5bb8b1f8c9b2b3d9fe9b19a18b4ca
Loïc Fejoz <loic@fejoz.net>
2021-08-31 22:59:35 +0200

    fix(constblock): Add powf test

--- tests/constblocks.rs
+++ tests/constblocks.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use futuresdr::blocks::ConstBlock;
 use futuresdr::blocks::ConstBuilder;
 use futuresdr::blocks::VectorSink;
 use futuresdr::blocks::VectorSinkBuilder;
@@ -109,6 +110,34 @@ fn mulconst_on_vec_complex_f32_source() -> Result<()> {
     assert_eq!(v.len(), orig.len());
     for (v_before, v_after) in orig.iter().zip(v) {
         assert!((v_after - (v_before * a_constant)).norm() < f32::EPSILON);
+    }
+
+    Ok(())
+}
+
+
+
+
+#[test]
+fn powerconst_on_vec_f32_source() -> Result<()> {
+    let a_constant: f32 = 4.0;
+    let mut fg = Flowgraph::new();
+    let orig: Vec<f32> = vec![1.0, 2.0, 3.5, 4.5, 10.5];
+    let src = fg.add_block(VectorSourceBuilder::<f32>::new(orig.clone()).build());
+    let add_const = fg.add_block(ConstBlock::new(a_constant, |v: f32, c: f32| v.powf(c)));
+    let vect_sink = fg.add_block(VectorSinkBuilder::<f32>::new().build());
+
+    fg.connect_stream(src, "out", add_const, "in")?;
+    fg.connect_stream(add_const, "out", vect_sink, "in")?;
+
+    fg = Runtime::new().run(fg)?;
+
+    let snk = fg.block_async::<VectorSink<f32>>(vect_sink).unwrap();
+    let v = snk.items();
+
+    assert_eq!(v.len(), orig.len());
+    for (v_before, v_after) in orig.iter().zip(v) {
+        assert!((v_after - v_before.powf(a_constant)).abs() < f32::EPSILON);
     }
 
     Ok(())
