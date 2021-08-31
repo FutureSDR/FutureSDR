99b8616801fca68c94bebcd28e9c550326389868
Loïc Fejoz <loic@fejoz.net>
2021-08-31 22:38:02 +0200

    fix(constblock): Add Multiply by constant block
    Refactor code so as to share between AddConst et MulConst blocks.
    Add common ConstBlockBuilder.

--- src/blocks/constblock.rs
+++ src/blocks/constblock.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use std::cmp;
 use std::ops::Add;
+use std::ops::Mul;
 
 use crate::runtime::AsyncKernel;
 use crate::runtime::Block;
@@ -12,36 +13,35 @@ use crate::runtime::StreamIo;
 use crate::runtime::StreamIoBuilder;
 use crate::runtime::WorkIo;
 
-pub struct AddConst<D> {
+pub struct ConstBlock<D, F: FnMut(D, D) -> D> {
     constant: D,
-    // item_size: usize,
+    f: F,
 }
 
-impl<D> AddConst<D>
+impl<D, F> ConstBlock<D, F>
 where
-    D: std::marker::Send + 'static + Add<Output = D> + std::marker::Copy,
+    D: std::marker::Send + 'static + Add<Output = D> + Mul<Output = D> + std::marker::Copy,
+    F: FnMut(D, D) -> D + std::marker::Send + 'static,
 {
-    pub fn new(constant: D) -> Block {
+    pub fn new(constant: D, f: F) -> Block {
         let item_size: usize = std::mem::size_of::<D>();
         Block::new_async(
-            BlockMetaBuilder::new("AddConst").build(),
+            BlockMetaBuilder::new("ConstBlock").build(),
             StreamIoBuilder::new()
                 .add_stream_input("in", item_size)
                 .add_stream_output("out", item_size)
                 .build(),
-            MessageIoBuilder::<AddConst<D>>::new().build(),
-            AddConst {
-                constant,
-                // item_size,
-            },
+            MessageIoBuilder::<ConstBlock<D, F>>::new().build(),
+            ConstBlock { constant, f },
         )
     }
 }
 
 #[async_trait]
-impl<D: std::marker::Send> AsyncKernel for AddConst<D>
+impl<D, F> AsyncKernel for ConstBlock<D, F>
 where
-    D: Add<Output = D> + 'static + std::marker::Copy,
+    D: std::marker::Send + Add<Output = D> + 'static + std::marker::Copy + Mul<Output = D>,
+    F: FnMut(D, D) -> D + std::marker::Send,
 {
     async fn work(
         &mut self,
@@ -56,10 +56,8 @@ where
         let mut m = 0;
         if !i.is_empty() && !o.is_empty() {
             m = cmp::min(i.len(), o.len());
-            let i_plus_const = i.iter().map(|vi: &D| *vi + self.constant);
-            /*unsafe {
-                ptr::copy_nonoverlapping(i_plus_const.as_ptr(), o.as_mut_ptr(), m);
-            }*/
+            let f_curry = |vi: &D| (self.f)(*vi, self.constant);
+            let i_plus_const = i.iter().map(f_curry);
             for (v, t) in i_plus_const.zip(o) {
                 *t = v;
             }
@@ -73,5 +71,29 @@ where
         }
 
         Ok(())
+    }
+}
+pub struct ConstBuilder<D> {
+    constant: D,
+}
+
+impl<D> ConstBuilder<D>
+where
+    D: std::marker::Send + 'static + Add<Output = D> + Mul<Output = D> + std::marker::Copy,
+{
+    pub fn new(constant: D) -> ConstBuilder<D> {
+        ConstBuilder { constant }
+    }
+
+    pub fn adder(a: D, b: D) -> D {
+        a + b
+    }
+
+    pub fn build_add(self) -> Block {
+        ConstBlock::new(self.constant, |a: D, b: D| a + b)
+    }
+
+    pub fn build_multiply(self) -> Block {
+        ConstBlock::new(self.constant, |a: D, b: D| -> D { a * b })
     }
 }
