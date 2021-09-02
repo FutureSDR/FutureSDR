ec566305aa5fb66f92c49cc8d60305c24f24537a
Bastian Bloessl <mail@bastibl.net>
2021-09-02 14:45:19 +0200

    config: cleanup custom settings

--- examples/zynq.sh
+++ examples/zynq.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cargo install cross
-RUSTFLAGS="-C target-cpu=generic" cross build --example zynq --target=aarch64-unknown-linux-gnu --no-default-features --features=zynq
+cross build --example zynq --target=aarch64-unknown-linux-gnu --no-default-features --features=zynq
