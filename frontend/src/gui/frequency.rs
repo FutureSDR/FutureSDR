d261be8d577451122a8205632f2fe1f7949064f9
Bastian Bloessl <mail@bastibl.net>
2021-09-08 09:49:29 +0200

    gui: waterfall color scheme

--- frontend/src/gui/frequency.rs
+++ frontend/src/gui/frequency.rs
@@ -118,9 +118,17 @@ precision mediump float;
 
 varying float power;
 
+// All components are in the range [0…1], including hue.
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 void main()
 {
-    gl_FragColor = mix(vec4(0, 0, 0, 0), vec4(0.4, 0.4, 0.4, 1), power);
+    gl_FragColor = vec4(hsv2rgb(vec3(power, .7, 0.7)), power);
 }
         "#;
         let frag_shader = gl.create_shader(GL::FRAGMENT_SHADER).unwrap();
