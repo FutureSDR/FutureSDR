546852cd8df843fe2f8996b5557a679cca42a7e7
Bastian Bloessl <mail@bastibl.net>
2021-11-03 18:27:14 +0100

    runtime: reexport async_trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
 #![recursion_limit = "512"]
 #![allow(clippy::new_ret_no_self)]
 
-#[macro_use]
-extern crate async_trait;
-
 pub mod blocks;
 pub mod runtime;
 
 // re-exports
 #[macro_use]
 pub extern crate log;
+#[macro_use]
+pub extern crate async_trait;
 
 pub use anyhow;
 pub use num_complex;
