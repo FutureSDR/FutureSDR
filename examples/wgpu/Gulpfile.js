96c0e802399525e2169231dfee49f47156cb27bb
Bastian Bloessl <mail@bastibl.net>
2021-12-21 10:56:07 +0100

    wasm/wgpu: simplify examples

--- examples/wgpu/Gulpfile.js
+++ examples/wgpu/Gulpfile.js
@@ -1,23 +1,6 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps   = require('gulp-sourcemaps');
-var cssmin = require('gulp-cssmin');
-var postcss = require('gulp-postcss');
-var debug = require('gulp-debug');
-var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-
-gulp.task('assets:css', function() {
-    return gulp.src('assets/css/futuresdr.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(postcss([autoprefixer()]))
-        .pipe(cssmin())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.stream());
-});
 
 gulp.task('assets:static', function() {
     return gulp.src('assets/static/**/*')
@@ -25,12 +8,10 @@ gulp.task('assets:static', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('assets', gulp.parallel('assets:css', 'assets:static'));
-gulp.task('default', gulp.parallel('assets'));
+gulp.task('default', gulp.task('assets:static'));
 
 gulp.task('serve', function() {
 
-    gulp.watch('assets/css/**/*', gulp.task('assets:css'));
     gulp.watch('assets/static/**/*', gulp.task('assets:static'));
 
     browserSync({
