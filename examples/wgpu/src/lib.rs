60ae8bf4531e516ab948f31f317811ee98ee02c8
Bastian Bloessl <mail@bastibl.net>
2021-12-21 10:01:58 +0100

    wgpu: add example

---
+++ examples/wgpu/src/lib.rs
@@ -0,0 +1,53 @@
+use std::iter::repeat_with;
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+use futuresdr::anyhow::{Context, Result};
+use futuresdr::log::info;
+use futuresdr::blocks::VectorSink;
+use futuresdr::blocks::VectorSourceBuilder;
+use futuresdr::blocks::Wgpu;
+use futuresdr::runtime::Flowgraph;
+use futuresdr::runtime::Runtime;
+use futuresdr::runtime::buffer::wgpu;
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+pub async fn run_fg() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    console_log::init().expect("could not initialize logger");
+    run().await.unwrap();
+}
+
+pub async fn run() -> Result<()> {
+    let n_items = 123123;
+    let orig: Vec<f32> = repeat_with(rand::random::<f32>).take(n_items).collect();
+
+    let mut fg = Flowgraph::new();
+
+    let src = VectorSourceBuilder::<f32>::new(orig.clone()).build();
+    let broker = wgpu::Broker::new().await;
+    let mul = Wgpu::new(broker, 4096);
+    let snk = VectorSink::<f32>::new(1024);
+
+    let src = fg.add_block(src);
+    let mul = fg.add_block(mul);
+    let snk = fg.add_block(snk);
+
+    fg.connect_stream_with_type(src, "out", mul, "in", wgpu::H2D::new())?;
+    fg.connect_stream_with_type(mul, "out", snk, "in", wgpu::D2H::new())?;
+
+    info!("start flowgraph");
+    fg = Runtime::new().run_async(fg).await?;
+
+    let snk = fg.block_async::<VectorSink<f32>>(snk).context("wrong block type")?;
+    let v = snk.items();
+
+    assert_eq!(v.len(), n_items);
+    for i in 0..v.len() {
+        assert!((orig[i] * 12.0 - v[i]).abs() < f32::EPSILON);
+    }
+
+    info!("data matches");
+    Ok(())
+}
