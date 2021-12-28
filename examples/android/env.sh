6331d80f701b0b5ec226fe8147362e20dd99ad92
Bastian Bloessl <mail@bastibl.net>
2021-12-28 12:00:19 +0100

    android: fix env.sh script

--- examples/android/env.sh
+++ examples/android/env.sh
@@ -1,11 +1,14 @@
 export TOOLCHAIN_ROOT=${HOME}/Android/Sdk/ndk/21.3.6528147
 export HOST_ARCH=linux-x86_64
+export BUILD_ROOT=/home/basti/src/gnuradio-android
 
 #############################################################
 ### DERIVED CONFIG
 #############################################################
 export SYS_ROOT=${TOOLCHAIN_ROOT}/sysroot
-export PKG_CONFIG_SYSROOT_DIR=${SYS_ROOT}
+# the variable has to be set to allow cross-compilation
+# but the paths in .pc files are absolute
+export PKG_CONFIG_SYSROOT_DIR=/
 export TOOLCHAIN_BIN=${TOOLCHAIN_ROOT}/toolchains/llvm/prebuilt/${HOST_ARCH}/bin
 export API_LEVEL=29
 export CC="${TOOLCHAIN_BIN}/aarch64-linux-android${API_LEVEL}-clang"
@@ -13,7 +16,6 @@ export LD=${TOOLCHAIN_BIN}/aarch64-linux-android-ld
 export AR=${TOOLCHAIN_BIN}/aarch64-linux-android-ar
 export RANLIB=${TOOLCHAIN_BIN}/aarch64-linux-android-ranlib
 export STRIP=${TOOLCHAIN_BIN}/aarch64-linux-android-strip
-export BUILD_ROOT=$(dirname $(readlink -f "$0"))
 export PATH=${TOOLCHAIN_BIN}:${PATH}
 export PREFIX=${BUILD_ROOT}/toolchain/arm64-v8a
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
