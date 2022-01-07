acf0bf71716da26ba0db19dff0d6ee2aa341028a
Bastian Bloessl <mail@bastibl.net>
2022-01-07 10:30:42 +0100

    example: local server, relax cors

--- examples/wgpu/server.py
+++ examples/wgpu/server.py
@@ -5,8 +5,6 @@ import sys
 class CORSRequestHandler (SimpleHTTPRequestHandler):
     def end_headers (self):
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
-        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
