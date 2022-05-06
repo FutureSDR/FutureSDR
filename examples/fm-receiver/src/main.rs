3f0423a4d0f953a5fbb39b72facd461d79e28802
JuliDi <20155974+JuliDi@users.noreply.github.com>
2022-05-06 17:05:14 +0200

    Add more doc, remove "head" block

--- examples/fm-receiver/src/main.rs
+++ examples/fm-receiver/src/main.rs
@@ -1,3 +1,15 @@
+//! A simple FM receiver that you can tune to nearby radio stations
+//! 
+//! When you run the example, it will build a flowgraph consisting of the following blocks:
+//! * SoapySource: Gets data from your SDR using the SoapySDR driver
+//! * Demodulator: Demodulates the FM signal
+//! * AudioSink: Plays the demodulated signal on your device
+//! 
+//! After giving it some time to start up the SDR, it enters a loop where you will
+//! be periodically asked to enter a new frequency that the SDR will be tuned to.
+//! **Watch out** though: Some frequencies (very high or very low) might be unsupported
+//! by your SDR and may cause a crash.
+
 use std::thread;
 use futuresdr::async_io;
 use futuresdr::blocks::audio::AudioSink;
@@ -46,12 +58,10 @@ fn main() -> ! {
     let src = fg.add_block(src);
     let demod = fg.add_block(demod);
     let snk = fg.add_block(snk);
-    let head = fg.add_block(Head::<f32>::new(20_000_000));
 
     // ... and connect the ports appropriately
     fg.connect_stream(src, "out", demod, "in").unwrap();
-    fg.connect_stream(demod, "out", head, "in").unwrap();
-    fg.connect_stream(head, "out", snk, "in").unwrap();
+    fg.connect_stream(demod, "out", snk, "in").unwrap();
 
 
     // Start the flowgraph and save the handle
