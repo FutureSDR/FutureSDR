3195f588714c5cb475f69fdfe38a28b07d88671b
Bastian Bloessl <mail@bastibl.net>
2022-06-06 14:08:30 +0200

    android-hw: update env to ndk 24.xx

--- examples/android-hw/env.sh
+++ examples/android-hw/env.sh
@@ -1,4 +1,4 @@
-export TOOLCHAIN_ROOT=${HOME}/Android/Sdk/ndk/21.3.6528147
+export TOOLCHAIN_ROOT=${HOME}/Android/Sdk/ndk/24.0.8215888
 export HOST_ARCH=linux-x86_64
 export BUILD_ROOT=/home/basti/src/gnuradio-android
 
@@ -12,14 +12,14 @@ export PKG_CONFIG_SYSROOT_DIR=/
 export TOOLCHAIN_BIN=${TOOLCHAIN_ROOT}/toolchains/llvm/prebuilt/${HOST_ARCH}/bin
 export API_LEVEL=29
 export CC="${TOOLCHAIN_BIN}/aarch64-linux-android${API_LEVEL}-clang"
-export LD=${TOOLCHAIN_BIN}/aarch64-linux-android-ld
-export AR=${TOOLCHAIN_BIN}/aarch64-linux-android-ar
-export RANLIB=${TOOLCHAIN_BIN}/aarch64-linux-android-ranlib
+export LD=${TOOLCHAIN_BIN}/ld
+export AR=${TOOLCHAIN_BIN}/llvm-ar
+export RANLIB=${TOOLCHAIN_BIN}/llvm-ranlib
 export STRIP=${TOOLCHAIN_BIN}/aarch64-linux-android-strip
 export PATH=${TOOLCHAIN_BIN}:${PATH}
 export PREFIX=${BUILD_ROOT}/toolchain/arm64-v8a
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
 export NCORES=$(getconf _NPROCESSORS_ONLN)
 
-export TARGET_CC="${TOOLCHAIN_BIN}/aarch64-linux-android${API_LEVEL}-clang"
-export TARGET_AR=${TOOLCHAIN_BIN}/aarch64-linux-android-ar
+export TARGET_CC=${CC}
+export TARGET_AR=${AR}
