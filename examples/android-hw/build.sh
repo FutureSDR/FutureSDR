a1dc14abb9e6495df4fc294dbe354dcfeaff1b96
Bastian Bloessl <mail@bastibl.net>
2022-06-07 12:02:01 +0200

    android: support ndk 23+

--- examples/android-hw/build.sh
+++ examples/android-hw/build.sh
@@ -7,4 +7,4 @@ source ./env.sh
 ln -s ${PREFIX}/lib/SoapySDR/modules0.8/librtlsdrSupport.so ${PREFIX}/lib/ || true
 ln -s ${HOME}/.cargo/target/aarch64-linux-android/debug/libandroidhw.so ${PREFIX}/lib/ || true
 
-RUSTFLAGS="-C target-cpu=generic" cargo build --target aarch64-linux-android --lib
+RUSTFLAGS="-L $(pwd) -C target-cpu=generic" cargo build --target aarch64-linux-android --lib
