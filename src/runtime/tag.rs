b05a675dae70d7844c8a496114b0dfc5855e35ee
Bastian Bloessl <mail@bastibl.net>
2022-08-09 10:18:56 +0200

    tag: TagAny and Downcast

--- src/runtime/tag.rs
+++ src/runtime/tag.rs
@@ -6,8 +6,26 @@ use crate::runtime::Pmt;
 use crate::runtime::StreamInput;
 use crate::runtime::StreamOutput;
 
-pub trait TagAny: Any + Send + DynClone {}
+pub trait TagAny: Any + DynClone + Send + 'static {
+    fn as_any(&self) -> &dyn Any;
+}
 dyn_clone::clone_trait_object!(TagAny);
+
+impl<T: Any + DynClone + Send + 'static> TagAny for T {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+pub trait Downcast {
+    fn downcast_ref<T: TagAny>(&self) -> Option<&T>;
+}
+
+impl Downcast for &Box<dyn TagAny> {
+    fn downcast_ref<T: TagAny>(&self) -> Option<&T> {
+        (&***self).as_any().downcast_ref::<T>()
+    }
+}
 
 impl fmt::Debug for Box<dyn TagAny> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
