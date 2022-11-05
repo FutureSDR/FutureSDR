3d42e785ff949a2903d948fd1f28d40cf33f8153
Bastian Bloessl <mail@bastibl.net>
2022-11-05 17:13:46 +0100

    clap update

--- perf/buffer_size/buffer_size.rs
+++ perf/buffer_size/buffer_size.rs
@@ -1,4 +1,4 @@
-use clap::{Arg, Command};
+use clap::Parser;
 use std::time;
 
 use futuresdr::anyhow::{Context, Result};
@@ -30,114 +30,79 @@ fn connect(
     }
 }
 
-fn main() -> Result<()> {
-    let matches = Command::new("Buffer Type Performance")
-        .arg(
-            Arg::new("run")
-                .short('r')
-                .long("run")
-                .takes_value(true)
-                .value_name("RUN")
-                .default_value("0")
-                .help("Sets run number."),
-        )
-        .arg(
-            Arg::new("stages")
-                .short('s')
-                .long("stages")
-                .takes_value(true)
-                .value_name("STAGES")
-                .default_value("6")
-                .help("Sets the number of stages."),
-        )
-        .arg(
-            Arg::new("pipes")
-                .short('p')
-                .long("pipes")
-                .takes_value(true)
-                .value_name("PIPES")
-                .default_value("5")
-                .help("Sets the number of pipes."),
-        )
-        .arg(
-            Arg::new("samples")
-                .short('n')
-                .long("samples")
-                .takes_value(true)
-                .value_name("SAMPLES")
-                .default_value("15000000")
-                .help("Sets the number of samples."),
-        )
-        .arg(
-            Arg::new("buffer_size")
-                .long("buffer_size")
-                .takes_value(true)
-                .value_name("BYTES")
-                .default_value("65536")
-                .help("Minimum buffer size."),
-        )
-        .arg(
-            Arg::new("scheduler")
-                .short('S')
-                .long("scheduler")
-                .takes_value(true)
-                .value_name("SCHEDULER")
-                .default_value("smol1")
-                .help("Sets the scheduler."),
-        )
-        .arg(
-            Arg::new("slab")
-                .long("slab")
-                .takes_value(false)
-                .help("Use Slab buffers."),
-        )
-        .get_matches();
+#[derive(Parser, Debug)]
+#[clap(version)]
+struct Args {
+    #[clap(short, long, default_value_t = 0)]
+    run: usize,
+    #[clap(short, long, default_value_t = 6)]
+    stages: usize,
+    #[clap(short, long, default_value_t = 5)]
+    pipes: usize,
+    #[clap(short = 'n', long, default_value_t = 15000000)]
+    samples: usize,
+    #[clap(short, long, default_value_t = 65536)]
+    buffer_size: usize,
+    #[clap(short = 'S', long, default_value = "smol1")]
+    scheduler: String,
+    #[clap(long)]
+    slab: bool,
+}
 
-    let run: u32 = matches.value_of_t("run").context("no run")?;
-    let pipes: u32 = matches.value_of_t("pipes").context("no pipe")?;
-    let stages: u32 = matches.value_of_t("stages").context("no stages")?;
-    let samples: usize = matches.value_of_t("samples").context("no samples")?;
-    let buffer_size: usize = matches
-        .value_of_t("buffer_size")
-        .context("no buffer_size")?;
-    let scheduler: String = matches.value_of_t("scheduler").context("no scheduler")?;
-    let slab: bool = matches.is_present("slab");
+fn main() -> Result<()> {
+    let args = Args::parse();
 
     let mut fg = Flowgraph::new();
     let mut snks = Vec::new();
 
-    for _ in 0..pipes {
+    for _ in 0..args.pipes {
         let src = fg.add_block(NullSource::<f32>::new());
-        let head = fg.add_block(Head::<f32>::new(samples as u64));
-        connect(&mut fg, src, "out", head, "in", slab, buffer_size)?;
+        let head = fg.add_block(Head::<f32>::new(args.samples as u64));
+        connect(&mut fg, src, "out", head, "in", args.slab, args.buffer_size)?;
 
         let mut last = fg.add_block(CopyRand::<f32>::new(1024));
-        connect(&mut fg, head, "out", last, "in", slab, buffer_size)?;
+        connect(
+            &mut fg,
+            head,
+            "out",
+            last,
+            "in",
+            args.slab,
+            args.buffer_size,
+        )?;
 
-        for _ in 1..stages {
+        for _ in 1..args.stages {
             let block = fg.add_block(CopyRand::<f32>::new(1024));
-            connect(&mut fg, last, "out", block, "in", slab, buffer_size)?;
+            connect(
+                &mut fg,
+                last,
+                "out",
+                block,
+                "in",
+                args.slab,
+                args.buffer_size,
+            )?;
             last = block;
         }
 
         let snk = fg.add_block(NullSink::<f32>::new());
-        connect(&mut fg, last, "out", snk, "in", slab, buffer_size)?;
+        connect(&mut fg, last, "out", snk, "in", args.slab, args.buffer_size)?;
         snks.push(snk);
     }
 
     let elapsed;
 
-    if scheduler == "smol1" {
+    if args.scheduler == "smol1" {
         let runtime = Runtime::with_scheduler(SmolScheduler::new(1, false));
         let now = time::Instant::now();
         fg = runtime.run(fg)?;
         elapsed = now.elapsed();
-    } else if scheduler == "smoln" {
+    } else if args.scheduler == "smoln" {
         let runtime = Runtime::with_scheduler(SmolScheduler::default());
         let now = time::Instant::now();
         fg = runtime.run(fg)?;
         elapsed = now.elapsed();
-    } else if scheduler == "flow" {
+    } else if args.scheduler == "flow" {
         let runtime = Runtime::with_scheduler(FlowScheduler::new());
         let now = time::Instant::now();
         fg = runtime.run(fg)?;
@@ -149,18 +114,18 @@ fn main() -> Result<()> {
     for s in snks {
         let snk = fg.kernel::<NullSink<f32>>(s).context("no block")?;
         let v = snk.n_received();
-        assert_eq!(v, samples);
+        assert_eq!(v, args.samples);
     }
 
     println!(
         "{},{},{},{},{},{},{},{}",
-        run,
-        pipes,
-        stages,
-        samples,
-        buffer_size,
-        scheduler,
-        if slab { "slab" } else { "circ" },
+        args.run,
+        args.pipes,
+        args.stages,
+        args.samples,
+        args.buffer_size,
+        args.scheduler,
+        if args.slab { "slab" } else { "circ" },
         elapsed.as_secs_f64()
     );
 
