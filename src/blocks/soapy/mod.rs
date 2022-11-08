9e87fe81249904be5a7e0fb1acfde6f6ce6b5c28
Eric Schneider <erictschneider@gmail.com>
2022-11-08 14:06:41 -0700

    soapy: multi-channel configuration. Supports individual channel configuration via builder and runtime msg ports. Fixes: #85

---
+++ src/blocks/soapy/mod.rs
@@ -0,0 +1,303 @@
+use crate::{
+    anyhow::{bail, Context, Result},
+    runtime::Pmt,
+};
+use soapysdr::Direction::{Rx, Tx};
+use std::{
+    marker::PhantomData,
+    sync::{Arc, Mutex},
+};
+
+mod config;
+mod sink;
+mod source;
+
+pub use self::config::{SoapyConfig, SoapyConfigItem, SoapyDevSpec, SoapyDirection};
+pub use self::sink::{SoapySink, SoapySinkBuilder};
+pub use self::source::{SoapySource, SoapySourceBuilder};
+
+static SOAPY_INIT: async_lock::Mutex<()> = async_lock::Mutex::new(());
+
+pub struct SoapyDevice<T> {
+    dev: Option<soapysdr::Device>,
+    init_cfg: Arc<Mutex<config::SoapyInitConfig>>,
+    chans: Vec<usize>,
+    stream: Option<T>,
+}
+
+// Note: there is additional impl in [`Self::command`]
+impl<T> SoapyDevice<T> {
+    /// The handler for messages on the "cmd" port.
+    ///
+    /// [`default_dir`]: A default direction that is set by the block
+    /// to indicate if it is a source or sink. This is only a default, some
+    /// messages may specify different directions, regardless of the natural
+    /// direction of the block.
+    ///
+    fn base_cmd_handler(&mut self, pmt: Pmt, default_dir: &SoapyDirection) -> Result<Pmt> {
+        match SoapyConfig::try_from(pmt) {
+            Ok(cfg) => {
+                self.apply_config(&cfg, default_dir)?;
+                Ok(Pmt::Null)
+            }
+            Err(e) => bail!(e),
+        }
+    }
+
+    // For backwards compatibility, can only set the first stream channel
+    fn set_freq(&mut self, p: Pmt, default_dir: &SoapyDirection) -> Result<Pmt> {
+        let dev = self.dev.as_mut().context("no dev")?;
+
+        let freq = config::pmt_to_f64(&p)?;
+
+        if default_dir.is_rx(&SoapyDirection::None) {
+            dev.set_frequency(Rx, self.chans[0], freq, ())?;
+        }
+        if default_dir.is_tx(&SoapyDirection::None) {
+            dev.set_frequency(Tx, self.chans[0], freq, ())?;
+        }
+        Ok(Pmt::Null)
+    }
+
+    // For backwards compatibility, can only set the first stream channel
+    fn set_gain(&mut self, p: Pmt, default_dir: &SoapyDirection) -> Result<Pmt> {
+        let dev = self.dev.as_mut().context("no dev")?;
+
+        let gain = config::pmt_to_f64(&p)?;
+
+        if default_dir.is_rx(&SoapyDirection::None) {
+            dev.set_gain(Rx, self.chans[0], gain)?;
+        }
+        if default_dir.is_tx(&SoapyDirection::None) {
+            dev.set_gain(Tx, self.chans[0], gain)?;
+        }
+        Ok(Pmt::Null)
+    }
+
+    fn apply_config(&mut self, cfg: &SoapyConfig, default_dir: &SoapyDirection) -> Result<()> {
+        use SoapyConfigItem as SCI;
+
+        let opt_dev = self.dev.clone();
+
+        let dev = match opt_dev {
+            None => {
+                bail!("attempted apply_config without device");
+            }
+            Some(d) => d,
+        };
+
+        // The channels to which configuration items will apply.
+        // This defaults to all device channels, but can be modified
+        // with the "Channel" configuration item.
+        let mut chans = self.chans.clone();
+
+        let update_dir_fn = |d: &SoapyDirection| -> Vec<soapysdr::Direction> {
+            match (d.is_rx(default_dir), d.is_tx(default_dir)) {
+                (false, true) => vec![Tx],
+                (true, false) => vec![Rx],
+                (true, true) => vec![Rx, Tx],
+                _ => vec![],
+            }
+        };
+
+        let mut dir_flags = update_dir_fn(default_dir);
+
+        debug!("initial dir:{:?} chans:{:?})", dir_flags, chans);
+
+        for ci in &cfg.0 {
+            match ci {
+                SCI::Antenna(a) => {
+                    for d in dir_flags.iter() {
+                        for c in chans.iter() {
+                            dev.set_antenna(*d, *c, a.as_bytes())?;
+                        }
+                    }
+                }
+                SCI::Bandwidth(bw) => {
+                    for d in dir_flags.iter() {
+                        for c in chans.iter() {
+                            dev.set_bandwidth(*d, *c, *bw)?;
+                        }
+                    }
+                }
+                SCI::Channels(None) => {
+                    //All configured channels
+                    chans = self.chans.clone();
+                }
+                SCI::Channels(Some(c)) => {
+                    chans = c.clone();
+                }
+                SCI::Direction(d) => {
+                    dir_flags = update_dir_fn(d);
+                }
+                SCI::Freq(freq) => {
+                    for d in dir_flags.iter() {
+                        for c in chans.iter() {
+                            debug!("dev.set_frequency({:?},{},{})", *d, *c, *freq);
+                            dev.set_frequency(*d, *c, *freq, ())?;
+                        }
+                    }
+                }
+                SCI::Gain(gain) => {
+                    for d in dir_flags.iter() {
+                        for c in chans.iter() {
+                            debug!("dev.set_gain({:?},{},{})", *d, *c, *gain);
+                            dev.set_gain(*d, *c, *gain)?;
+                        }
+                    }
+                }
+                SCI::SampleRate(rate) => {
+                    for d in dir_flags.iter() {
+                        for c in chans.iter() {
+                            debug!("dev.set_sample_rate({:?},{},{})", *d, *c, *rate);
+                            dev.set_sample_rate(*d, *c, *rate)?;
+                        }
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn apply_init_config(&mut self, default_dir: &SoapyDirection) -> Result<()> {
+        let cfg_mtx = &self.init_cfg.clone();
+        let cfg = cfg_mtx.lock().unwrap();
+
+        match &cfg.dev {
+            SoapyDevSpec::Dev(d) => {
+                self.dev = Some(d.clone());
+            }
+            SoapyDevSpec::Filter(f) => {
+                let dev = soapysdr::Device::new(f.as_str());
+                match dev {
+                    Ok(d) => {
+                        self.dev = Some(d);
+                    }
+                    Err(e) => {
+                        bail!("Soapy device init error: {}", e);
+                    }
+                };
+            }
+        };
+        self.chans = cfg.chans.clone();
+        self.apply_config(&cfg.config, default_dir)?;
+        Ok(())
+    }
+}
+
+// unsafe impl<T> Sync for SoapyDevice<T> {}
+
+pub struct SoapyDevBuilder<T> {
+    init_cfg: config::SoapyInitConfig,
+    _phantom: PhantomData<T>,
+}
+
+/// A generic builder that is used for both source and sink.
+///
+/// Multiple channel configuration uses the [`Self::cfg_channels()`] method to
+/// control which channels *subsequent* methods will apply to, just like
+/// [`SoapyConfig`] (which is used internally here).
+impl<T> SoapyDevBuilder<T> {
+    /// Apply any required modifications for backwards compatibility and ease of use.
+    ///
+    /// Each `build()` will call this.
+    ///
+    /// - Add a default chan(0) if none exists.
+    /// - - This supports cases that do not call `chan` before other settings (or at all).
+    fn fixup(&mut self) {
+        if self.init_cfg.chans.is_empty() {
+            self.init_cfg.chans.push(0);
+        }
+    }
+
+    /// Specify a device using a filter string.
+    ///
+    /// See [`Self::device()`] for a more flexible option.
+    pub fn filter<S>(mut self, filter: S) -> SoapyDevBuilder<T>
+    where
+        S: Into<String>,
+    {
+        self.init_cfg.dev = SoapyDevSpec::Filter(filter.into());
+        self
+    }
+
+    /// Specify the soapy device.
+    ///
+    /// See: [`SoapyDevSpec`] and [`soapysdr::Device::new()`]
+    pub fn device(mut self, dev_spec: SoapyDevSpec) -> SoapyDevBuilder<T> {
+        self.init_cfg.dev = dev_spec;
+        self
+    }
+
+    /// Specify the device channels to be activated.
+    ///
+    /// If not specified a single channel (0) will be assumed.
+    pub fn dev_channels(mut self, chans: Vec<usize>) -> SoapyDevBuilder<T> {
+        self.init_cfg.chans = chans;
+        self
+    }
+
+    /// Set the stream activation time.
+    ///
+    /// The value should be relative to the value returned from
+    /// [`soapysdr::Device::get_hardware_time()`]
+    pub fn activate_time(mut self, time_ns: i64) -> SoapyDevBuilder<T> {
+        self.init_cfg.activate_time = Some(time_ns);
+        self
+    }
+
+    // ////////////////////////////////////////////////
+    // Runtime modifiable parameters below this point (e.g. via message ports)
+
+    /// Specify channels for *subsequent* configuration items.
+    ///
+    /// This allows different configuration items to be applied to different
+    /// channels.
+    ///
+    /// By default, configurations items are applied to *all* channels.
+    pub fn cfg_channels(mut self, chans: Option<Vec<usize>>) -> SoapyDevBuilder<T> {
+        self.init_cfg.config.push(SoapyConfigItem::Channels(chans));
+        self
+    }
+
+    /// Specify a *single* channel for *subsequent* configuration items.
+    ///
+    /// This is just a convenience wrapper around [`Self::cfg_channels`].
+    pub fn cfg_channel(mut self, chan: usize) -> SoapyDevBuilder<T> {
+        self.init_cfg
+            .config
+            .push(SoapyConfigItem::Channels(Some(vec![chan])));
+        self
+    }
+
+    /// See [`soapysdr::Device::set_antenna()`]
+    pub fn antenna<S>(mut self, antenna: S) -> SoapyDevBuilder<T>
+    where
+        S: Into<String>,
+    {
+        self.init_cfg
+            .config
+            .push(SoapyConfigItem::Antenna(antenna.into()));
+        self
+    }
+
+    /// See [`soapysdr::Device::set_frequency()`]
+    pub fn freq(mut self, freq: f64) -> SoapyDevBuilder<T> {
+        self.init_cfg.config.push(SoapyConfigItem::Freq(freq));
+        self
+    }
+
+    /// See [`soapysdr::Device::set_gain()`]
+    pub fn gain(mut self, gain: f64) -> SoapyDevBuilder<T> {
+        self.init_cfg.config.push(SoapyConfigItem::Gain(gain));
+        self
+    }
+
+    /// See [`soapysdr::Device::set_sample_rate()`]
+    pub fn sample_rate(mut self, sample_rate: f64) -> SoapyDevBuilder<T> {
+        self.init_cfg
+            .config
+            .push(SoapyConfigItem::SampleRate(sample_rate));
+        self
+    }
+}
---
+++ src/blocks/soapy/sink.rs
@@ -0,0 +1,208 @@
+use super::*;
+use crate::{
+    anyhow::{Context, Result},
+    num_complex::Complex32,
+    runtime::{
+        Block, BlockMeta, BlockMetaBuilder, Kernel, MessageIo, MessageIoBuilder, Pmt, StreamIo,
+        StreamIoBuilder, WorkIo,
+    },
+};
+use futures::{Future, FutureExt};
+use std::{
+    cmp,
+    marker::PhantomData,
+    pin::Pin,
+    sync::{Arc, Mutex},
+};
+
+pub type SoapySink = SoapyDevice<soapysdr::TxStream<Complex32>>;
+
+impl SoapySink {
+    fn new(init_cfg: config::SoapyInitConfig) -> Block {
+        let mut chans = init_cfg.chans.clone();
+        if chans.is_empty() {
+            chans.push(0);
+        }
+
+        let mut siob = StreamIoBuilder::new();
+
+        for i in 0..chans.len() {
+            if i == 0 {
+                // Never number the first output port for compatibility with single port instances
+                siob = siob.add_input::<Complex32>("in");
+            } else {
+                siob = siob.add_input::<Complex32>(&format!("in{}", i + 1));
+            }
+        }
+
+        Block::new(
+            BlockMetaBuilder::new("SoapySink").blocking().build(),
+            siob.build(),
+            MessageIoBuilder::new()
+                .add_input("freq", Self::on_freq_port)
+                .add_input("gain", Self::on_gain_port)
+                .add_input("cmd", Self::on_cmd_port)
+                .build(),
+            Self {
+                dev: None,
+                init_cfg: Arc::new(Mutex::new(init_cfg)),
+                chans,
+                stream: None,
+            },
+        )
+    }
+
+    fn on_cmd_port<'a>(
+        &'a mut self,
+        _mio: &'a mut MessageIo<Self>,
+        _meta: &'a mut BlockMeta,
+        p: Pmt,
+    ) -> Pin<Box<dyn Future<Output = Result<Pmt>> + Send + 'a>> {
+        async move { self.base_cmd_handler(p, &SoapyDirection::Tx) }.boxed()
+    }
+
+    fn on_freq_port<'a>(
+        &'a mut self,
+        _mio: &'a mut MessageIo<Self>,
+        _meta: &'a mut BlockMeta,
+        p: Pmt,
+    ) -> Pin<Box<dyn Future<Output = Result<Pmt>> + Send + 'a>> {
+        async move { self.set_freq(p, &SoapyDirection::Tx) }.boxed()
+    }
+
+    fn on_gain_port<'a>(
+        &'a mut self,
+        _mio: &'a mut MessageIo<Self>,
+        _meta: &'a mut BlockMeta,
+        p: Pmt,
+    ) -> Pin<Box<dyn Future<Output = Result<Pmt>> + Send + 'a>> {
+        async move { self.set_gain(p, &SoapyDirection::Tx) }.boxed()
+    }
+}
+
+#[doc(hidden)]
+#[async_trait]
+impl Kernel for SoapySink {
+    async fn work(
+        &mut self,
+        io: &mut WorkIo,
+        sio: &mut StreamIo,
+        _mio: &mut MessageIo<Self>,
+        _meta: &mut BlockMeta,
+    ) -> Result<()> {
+        let ins = sio.inputs_mut();
+        let full_bufs: Vec<&[Complex32]> = ins.iter_mut().map(|b| b.slice::<Complex32>()).collect();
+
+        let min_in_len = full_bufs.iter().map(|b| b.len()).min().unwrap_or(0);
+
+        let stream = self.stream.as_mut().unwrap();
+        let n = cmp::min(min_in_len, stream.mtu().unwrap());
+        if n == 0 {
+            return Ok(());
+        }
+
+        // Make a collection of same (minimum) size slices
+        let bufs: Vec<&[Complex32]> = full_bufs.iter().map(|b| &b[0..n]).collect();
+        let len = stream.write(&bufs, None, false, 1_000_000)?;
+
+        let mut finished = false;
+        for i in 0..ins.len() {
+            sio.input(i).consume(len);
+            if sio.input(i).finished() {
+                finished = true;
+            }
+        }
+        if len != min_in_len {
+            io.call_again = true;
+        } else if finished {
+            io.finished = true;
+        }
+        Ok(())
+    }
+
+    async fn init(
+        &mut self,
+        _sio: &mut StreamIo,
+        _mio: &mut MessageIo<Self>,
+        _meta: &mut BlockMeta,
+    ) -> Result<()> {
+        let _ = super::SOAPY_INIT.lock();
+        soapysdr::configure_logging();
+        if let Err(e) = self.apply_init_config(&SoapyDirection::Tx) {
+            warn!("SoapySink::new() apply_init_config error: {}", e);
+        }
+
+        let dev = self.dev.as_ref().context("no dev")?;
+        let cfg_mtx = &self.init_cfg.clone();
+        let cfg = cfg_mtx.lock().unwrap();
+
+        self.stream = Some(dev.tx_stream::<Complex32>(&self.chans)?);
+        self.stream
+            .as_mut()
+            .context("no stream")?
+            .activate(cfg.activate_time)?;
+
+        Ok(())
+    }
+
+    async fn deinit(
+        &mut self,
+        _sio: &mut StreamIo,
+        _mio: &mut MessageIo<Self>,
+        _meta: &mut BlockMeta,
+    ) -> Result<()> {
+        self.stream
+            .as_mut()
+            .context("no stream")?
+            .deactivate(None)?;
+        Ok(())
+    }
+}
+
+/// Build a [SoapySink].
+///
+/// Most logic is implemented in the shared [`SoapyDevBuilder`].
+///
+/// # Inputs
+///
+/// - **Message** `cmd`: a [`Pmt`] representing a configuration update or other command. See: [`SoapyConfig`] and [`SoapyDevice::base_cmd_handler()`].
+///
+/// - **Stream** `in`: Stream of [`Complex32`] to transmit.
+///
+/// # Usage
+/// ```no_run
+/// use futuresdr::blocks::SoapySinkBuilder;
+/// use futuresdr::runtime::Flowgraph;
+///
+/// let mut fg = Flowgraph::new();
+///
+/// let source = fg.add_block(
+///     SoapySinkBuilder::new()
+///         .filter("device=hackrf")
+///         .sample_rate(1e6)
+///         .freq(100e9)
+///         .gain(10.0)
+///         .build()
+/// );
+/// ```
+pub type SoapySinkBuilder = SoapyDevBuilder<SoapySink>;
+
+impl SoapyDevBuilder<SoapySink> {
+    pub fn new() -> Self {
+        Self {
+            init_cfg: config::SoapyInitConfig::default(),
+            _phantom: PhantomData,
+        }
+    }
+
+    pub fn build(mut self) -> Block {
+        self.fixup();
+        SoapySink::new(self.init_cfg)
+    }
+}
+
+impl Default for SoapyDevBuilder<SoapySink> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
