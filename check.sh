013f830de9a9de4cdfcdf37fe6d4fcd4c543ed2f
Bastian Bloessl <mail@bastibl.net>
2022-12-07 18:32:41 +0100

    add agc to ci and tests

--- check.sh
+++ check.sh
@@ -26,6 +26,7 @@ cd ${SCRIPTPATH}/perf/wgpu && cargo fmt --check
 cd ${SCRIPTPATH}/perf/zynq && cargo fmt --check
 
 # examples
+cd ${SCRIPTPATH}/examples/agc && cargo fmt --check
 cd ${SCRIPTPATH}/examples/android && cargo fmt --check
 cd ${SCRIPTPATH}/examples/android-hw && cargo fmt --check
 cd ${SCRIPTPATH}/examples/audio && cargo fmt --check
@@ -71,6 +72,7 @@ cd ${SCRIPTPATH}/perf/wgpu && RUSTFLAGS='--cfg=web_sys_unstable_apis' cargo clip
 cd ${SCRIPTPATH}/perf/zynq && cargo clippy --all-targets -- -D warnings
 
 # examples
+cd ${SCRIPTPATH}/examples/agc && cargo clippy --all-targets -- -D warnings
 cd ${SCRIPTPATH}/examples/android && cargo clippy --all-targets -- -D warnings
 cd ${SCRIPTPATH}/examples/android-hw && cargo clippy --all-targets -- -D warnings
 cd ${SCRIPTPATH}/examples/audio && cargo clippy --all-targets -- -D warnings
@@ -113,6 +115,7 @@ cd ${SCRIPTPATH}/perf/wgpu && cargo test --all-targets
 cd ${SCRIPTPATH}/perf/zynq && cargo test --all-targets
 
 # examples
+cd ${SCRIPTPATH}/examples/agc && cargo test --all-targets
 cd ${SCRIPTPATH}/examples/android && cargo test --all-targets
 cd ${SCRIPTPATH}/examples/android-hw && cargo test --all-targets
 cd ${SCRIPTPATH}/examples/audio && cargo test --all-targets
