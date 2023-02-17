0c2eef9c2d4b2ca43aff45ae6e0d6a9c10200bdd
Bastian Bloessl <mail@bastibl.net>
2023-02-17 09:43:12 +0100

    update dependencies

--- examples/spectrum/src/wasm.rs
+++ examples/spectrum/src/wasm.rs
@@ -1,5 +1,6 @@
 use futuresdr::anyhow::Result;
 use futuresdr::blocks::Fft;
+use futuresdr::blocks::FftDirection;
 use futuresdr::blocks::WasmFreq;
 use futuresdr::blocks::WasmSdr;
 use futuresdr::runtime::buffer::slab::Slab;
@@ -7,10 +8,7 @@ use futuresdr::runtime::Flowgraph;
 use futuresdr::runtime::Runtime;
 use wasm_bindgen::prelude::*;
 
-use crate::lin2db_block;
-use crate::power_block;
-use crate::FftShift;
-use crate::Keep1InN;
+const FFT_SIZE: usize = 4096;
 
 #[wasm_bindgen]
 pub async fn run_fg() {
@@ -21,18 +19,19 @@ async fn run() -> Result<()> {
     let mut fg = Flowgraph::new();
 
     let src = fg.add_block(WasmSdr::new());
-    let fft = fg.add_block(Fft::new(2048));
-    let power = fg.add_block(power_block());
-    let log = fg.add_block(lin2db_block());
-    let shift = fg.add_block(FftShift::<f32>::new());
-    let keep = fg.add_block(Keep1InN::new(0.1, 40));
+    let fft = fg.add_block(Fft::with_options(
+        FFT_SIZE,
+        FftDirection::Forward,
+        true,
+        None,
+    ));
+    let power = fg.add_block(crate::lin2power_db());
+    let keep = fg.add_block(crate::Keep1InN::<FFT_SIZE>::new(0.1, 20));
     let snk = fg.add_block(WasmFreq::new());
 
     fg.connect_stream_with_type(src, "out", fft, "in", Slab::with_config(65536, 2, 0))?;
     fg.connect_stream_with_type(fft, "out", power, "in", Slab::with_config(65536, 2, 0))?;
-    fg.connect_stream_with_type(power, "out", log, "in", Slab::with_config(65536, 2, 0))?;
-    fg.connect_stream_with_type(log, "out", shift, "in", Slab::with_config(65536, 2, 0))?;
-    fg.connect_stream_with_type(shift, "out", keep, "in", Slab::with_config(65536, 2, 0))?;
+    fg.connect_stream_with_type(power, "out", keep, "in", Slab::with_config(65536, 2, 0))?;
     fg.connect_stream_with_type(keep, "out", snk, "in", Slab::with_config(65536, 2, 0))?;
 
     Runtime::new().run_async(fg).await?;
