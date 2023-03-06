840f2cc8f517a9540f3c6a87c602a7a8f9732fb8
Bastian Bloessl <mail@bastibl.net>
2023-03-06 12:48:27 +0100

    add pre-build frontend

---
+++ crates/frontend/dist/futuresdr_bg.wasm.d.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function add_time(a: number, b: number, c: number, d: number, e: number, f: number): void;
+export function futuresdr_init(): void;
+export function add_freq(a: number, b: number, c: number, d: number, e: number, f: number): void;
+export function add_flowgraph(a: number, b: number, c: number, d: number): void;
+export function add_slider_u32(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number): void;
+export function kitchen_sink(a: number, b: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__he962eafd7d459ee1(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1dd0805649b770c2(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hfe3a8b8c9b178c72(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__ha02848d70b1c5cda(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h13a22b4659c28630(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__he15557871ce10f07(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
