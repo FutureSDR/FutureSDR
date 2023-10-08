5dfbd763e2c3bb56ef484359a43a6ee7908d5b9c
Bastian Bloessl <mail@bastibl.net>
2023-10-08 21:00:47 +0200

    asdf

--- examples/m17/src/symbol_sync.rs
+++ examples/m17/src/symbol_sync.rs
@@ -30,7 +30,14 @@ struct ClockTrackingLoop {
 }
 
 impl ClockTrackingLoop {
-    fn new(loop_bw: f32, max_period: f32, min_period: f32, nominal_period: f32, damping: f32, ted_gain: f32) -> Self {
+    fn new(
+        loop_bw: f32,
+        max_period: f32,
+        min_period: f32,
+        nominal_period: f32,
+        damping: f32,
+        ted_gain: f32,
+    ) -> Self {
         let mut s = Self {
             avg_period: nominal_period,
             max_avg_period: max_period,
@@ -46,14 +53,14 @@ impl ClockTrackingLoop {
             prev_avg_period: nominal_period,
             prev_inst_period: nominal_period,
             prev_phase: 0.0,
-        }
+        };
 
         s.set_max_avg_period(max_period);
         s.set_min_avg_period(min_period);
         s.set_nom_avg_period(nominal_period);
 
-        s.set_avg_period(self.nom_avg_period);
-        s.set_inst_period(self.nom_avg_period);
+        s.set_avg_period(s.nom_avg_period);
+        s.set_inst_period(s.nom_avg_period);
 
         if s.zeta < 0.0 {
             panic!("clock_tracking_loop: damping factor must be > 0.0");
@@ -64,7 +71,7 @@ impl ClockTrackingLoop {
         }
 
         if s.ted_gain <= 0.0 {
-            panic!( "clock_tracking_loop: expected ted gain must be greater than 0.0");
+            panic!("clock_tracking_loop: expected ted gain must be greater than 0.0");
         }
         s.update_gains();
         s
@@ -75,7 +82,7 @@ impl ClockTrackingLoop {
         self.prev_inst_period = self.inst_period;
         self.prev_phase = self.phase;
         self.avg_period = self.avg_period + self.beta * error;
-        period_limit();
+        self.period_limit();
 
         self.inst_period = self.avg_period + self.alpha * error;
         if self.inst_period <= 0.0 {
@@ -95,11 +102,11 @@ impl ClockTrackingLoop {
         let period = self.avg_period;
         let limit = period / 2.0;
 
-        while (self.phase > limit) {
+        while self.phase > limit {
             self.phase -= period;
         }
 
-        while (self.phase <= -limit) {
+        while self.phase <= -limit {
             self.phase += period;
         }
     }
@@ -113,59 +120,57 @@ impl ClockTrackingLoop {
     }
 
     fn update_gains(&mut self) {
-        let omega_n_T;
-        let omega_d_T;
-        let zeta_omega_n_T;
-        let cosx_omega_d_T;
+        let omega_n_t;
+        let omega_d_t;
+        let zeta_omega_n_t;
+        let cosx_omega_d_t;
 
         let k0;
         let k1;
-        let sinh_zeta_omega_n_T;
+        let sinh_zeta_omega_n_t;
         let alpha;
         let beta;
 
-        omega_n_T = self.omega_n_norm;
-        zeta_omega_n_T = self.zeta * omega_n_T;
+        omega_n_t = self.omega_n_norm;
+        zeta_omega_n_t = self.zeta * omega_n_t;
         k0 = 2.0 / self.ted_gain;
-        k1 = (-zeta_omega_n_T).exp();
-        sinh_zeta_omega_n_T = (zeta_omega_n_T).sinh();
+        k1 = (-zeta_omega_n_t).exp();
+        sinh_zeta_omega_n_t = (zeta_omega_n_t).sinh();
 
         if self.zeta > 1.0 {
-            omega_d_T = omega_n_T * (self.zeta * self.zeta - 1.0).sqrt();
-            cosx_omega_d_T = omega_d_T.cosh();
-
+            omega_d_t = omega_n_t * (self.zeta * self.zeta - 1.0).sqrt();
+            cosx_omega_d_t = omega_d_t.cosh();
         } else if self.zeta == 1.0 {
-            omega_d_T = 0.0;
-            cosx_omega_d_T = 1.0;
-
+            omega_d_t = 0.0;
+            cosx_omega_d_t = 1.0;
         } else {
-            omega_d_T = omega_n_T * (1.0 - self.zeta * self.zeta).sqrt();
-            cosx_omega_d_T = omega_d_T.cos();
+            omega_d_t = omega_n_t * (1.0 - self.zeta * self.zeta).sqrt();
+            cosx_omega_d_t = omega_d_t.cos();
         }
 
-        alpha = k0 * k1 * sinh_zeta_omega_n_T;
-        beta = k0 * (1.0 - k1 * (sinh_zeta_omega_n_T + cosx_omega_d_T));
+        alpha = k0 * k1 * sinh_zeta_omega_n_t;
+        beta = k0 * (1.0 - k1 * (sinh_zeta_omega_n_t + cosx_omega_d_t));
 
-        set_alpha(alpha);
-        set_beta(beta);
+        self.set_alpha(alpha);
+        self.set_beta(beta);
     }
 
     fn set_loop_bandwidth(&mut self, bw: f32) {
         assert!(bw >= 0.0);
         self.omega_n_norm = bw;
-        update_gains();
+        self.update_gains();
     }
 
     fn set_damping_factor(&mut self, df: f32) {
         assert!(df >= 0.0);
         self.zeta = df;
-        update_gains();
+        self.update_gains();
     }
 
     fn set_ted_gain(&mut self, ted_gain: f32) {
         assert!(ted_gain > 0.0);
         self.ted_gain = ted_gain;
-        update_gains();
+        self.update_gains();
     }
 
     fn set_alpha(&mut self, alpha: f32) {
@@ -176,68 +181,70 @@ impl ClockTrackingLoop {
         self.beta = beta;
     }
 
-void clock_tracking_loop::set_avg_period(float period)
-{
-    d_avg_period = period;
-    d_prev_avg_period = period;
-}
-
-void clock_tracking_loop::set_inst_period(float period)
-{
-    d_inst_period = period;
-    d_prev_inst_period = period;
-}
-
-void clock_tracking_loop::set_phase(float phase)
-{
-    // This previous phase is likely inconsistent with the tracking,
-    // but if the caller is setting the phase, the odds of
-    // revert_loop() being called are slim.
-    d_prev_phase = phase;
-
-    d_phase = phase;
-}
-
-void clock_tracking_loop::set_max_avg_period(float period) { d_max_avg_period = period; }
-
-void clock_tracking_loop::set_min_avg_period(float period) { d_min_avg_period = period; }
-
-void clock_tracking_loop::set_nom_avg_period(float period)
-{
-    if (period < d_min_avg_period || period > d_max_avg_period) {
-        d_nom_avg_period = (d_max_avg_period + d_min_avg_period) / 2.0f;
-    } else {
-        d_nom_avg_period = period;
+    fn set_avg_period(&mut self, period: f32) {
+        self.avg_period = period;
+        self.prev_avg_period = period;
     }
-}
 
-/*******************************************************************
- * GET FUNCTIONS
- *******************************************************************/
+    fn set_inst_period(&mut self, period: f32) {
+        self.inst_period = period;
+        self.prev_inst_period = period;
+    }
 
-float clock_tracking_loop::get_loop_bandwidth() const { return d_omega_n_norm; }
+    fn set_phase(&mut self, phase: f32) {
+        self.prev_phase = phase;
+        self.phase = phase;
+    }
 
-float clock_tracking_loop::get_damping_factor() const { return d_zeta; }
+    fn set_max_avg_period(&mut self, period: f32) {
+        self.max_avg_period = period;
+    }
 
-float clock_tracking_loop::get_ted_gain() const { return d_ted_gain; }
+    fn set_min_avg_period(&mut self, period: f32) {
+        self.min_avg_period = period;
+    }
 
-float clock_tracking_loop::get_alpha() const { return d_alpha; }
+    fn set_nom_avg_period(&mut self, period: f32) {
+        if (period < self.min_avg_period) || (period > self.max_avg_period) {
+            self.nom_avg_period = (self.max_avg_period + self.min_avg_period) / 2.0;
+        } else {
+            self.nom_avg_period = period;
+        }
+    }
 
-float clock_tracking_loop::get_beta() const { return d_beta; }
-
-float clock_tracking_loop::get_avg_period() const { return d_avg_period; }
-
-float clock_tracking_loop::get_inst_period() const { return d_inst_period; }
-
-float clock_tracking_loop::get_phase() const { return d_phase; }
-
-float clock_tracking_loop::get_max_avg_period() const { return d_max_avg_period; }
-
-float clock_tracking_loop::get_min_avg_period() const { return d_min_avg_period; }
-
-float clock_tracking_loop::get_nom_avg_period() const { return d_nom_avg_period; }
-
-
+    fn get_loop_bandwidth(&self) -> f32 {
+        self.omega_n_norm
+    }
+    fn get_damping_factor(&self) -> f32 {
+        self.zeta
+    }
+    fn get_ted_gain(&self) -> f32 {
+        self.ted_gain
+    }
+    fn get_alpha(&self) -> f32 {
+        self.alpha
+    }
+    fn get_beta(&self) -> f32 {
+        self.beta
+    }
+    fn get_avg_period(&self) -> f32 {
+        self.avg_period
+    }
+    fn get_inst_period(&self) -> f32 {
+        self.inst_period
+    }
+    fn get_phase(&self) -> f32 {
+        self.phase
+    }
+    fn get_max_avg_period(&self) -> f32 {
+        self.max_avg_period
+    }
+    fn get_min_avg_period(&self) -> f32 {
+        self.min_avg_period
+    }
+    fn get_nom_avg_period(&self) -> f32 {
+        self.nom_avg_period
+    }
 }
 
 struct TimingErrorDetector {
@@ -252,8 +259,12 @@ struct TimingErrorDetector {
 }
 
 impl TimingErrorDetector {
-
-    fn new(inputs_per_symbol: usize, error_depth: usize, needs_lookahead: bool, needs_derivative: bool) -> Self {
+    fn new(
+        inputs_per_symbol: usize,
+        error_depth: usize,
+        needs_lookahead: bool,
+        needs_derivative: bool,
+    ) -> Self {
         let mut s = Self {
             error: 0.0,
             error_depth,
@@ -277,7 +288,6 @@ impl TimingErrorDetector {
         self.input.pop_back();
         assert_eq!(self.needs_derivative, false);
         assert_eq!(self.needs_lookahead, false);
-
 
         self.advance_input_clock();
         if self.input_clock == 0 {
@@ -305,7 +315,7 @@ impl TimingErrorDetector {
 
     fn revert(&mut self, preserve_error: bool) {
         if (self.input_clock == 0) && (preserve_error == false) {
-            self.error = self.prev_error; 
+            self.error = self.prev_error;
         }
         self.revert_input_clock();
 
@@ -319,7 +329,8 @@ impl TimingErrorDetector {
         self.error = 0.0;
         self.prev_error = 0.0;
 
-        self.input = VecDeque::from_iter(vec![Complex32::new(0.0, 0.0); self.error_depth].into_iter());
+        self.input =
+            VecDeque::from_iter(vec![Complex32::new(0.0, 0.0); self.error_depth].into_iter());
         self.sync_reset_input_clock();
     }
 
@@ -327,8 +338,7 @@ impl TimingErrorDetector {
         self.input_clock = (self.input_clock + 1) % self.inputs_per_symbol as i32;
     }
 
-    fn revert_input_clock(&mut self)
-    {
+    fn revert_input_clock(&mut self) {
         if self.input_clock == 0 {
             self.input_clock = self.inputs_per_symbol as i32 - 1;
         } else {
