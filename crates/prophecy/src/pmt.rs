7824f843d59ee071fab004b11c28ef0038e1b157
Bastian Bloessl <mail@bastibl.net>
2023-11-06 08:24:55 +0100

    asdf

--- crates/prophecy/src/pmt.rs
+++ crates/prophecy/src/pmt.rs
@@ -1,4 +1,5 @@
 use futuresdr::runtime::Pmt;
+use leptos::html::Input;
 use leptos::*;
 
 #[component]
@@ -29,5 +30,41 @@ pub fn Pmt(#[prop(into)] pmt: MaybeSignal<Pmt>) -> impl IntoView {
 
     view! {
         <span class=class>{ move || pmt.get().to_string() }</span>
+    }
+}
+
+const ENTER_KEY: u32 = 13;
+
+#[component]
+pub fn PmtInput(
+    set_pmt: WriteSignal<Pmt>,
+    #[prop(into, optional)] input_class: String,
+    #[prop(into, optional)] error_class: String,
+) -> impl IntoView {
+    let (error, error_set) = create_signal(None);
+    let error_memo = create_memo(move |_| error());
+
+    let input_ref = create_node_ref::<Input>();
+    let parse_pmt = move || {
+        let input = input_ref.get().unwrap();
+        let v = input.value();
+        if let Ok(p) = v.parse::<Pmt>() {
+            set_pmt(p);
+        } else {
+            error_set(Some(error_class.clone()));
+        }
+    };
+
+    let on_input = move |ev: web_sys::KeyboardEvent| {
+        ev.stop_propagation();
+        error_set(None);
+        let key_code = ev.key_code();
+        if key_code == ENTER_KEY {
+            parse_pmt();
+        }
+    };
+
+    view! {
+        <input class=input_class class=error_memo node_ref=input_ref on:keydown=on_input></input>
     }
 }
