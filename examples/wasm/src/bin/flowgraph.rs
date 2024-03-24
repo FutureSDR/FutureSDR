2ab05e021770878dbd560565b620aa717325c37e
Bastian Bloessl <mail@bastibl.net>
2024-03-24 07:17:06 +0100

    fmt

--- examples/wasm/src/bin/flowgraph.rs
+++ examples/wasm/src/bin/flowgraph.rs
@@ -7,9 +7,7 @@ fn main() -> futuresdr::anyhow::Result<()> {
 pub fn main() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     console_log::init().expect("could not initialize logger");
-    wasm_bindgen_futures::spawn_local(
-        async {
-            wasm::run().await.unwrap();
-        }
-    );
+    wasm_bindgen_futures::spawn_local(async {
+        wasm::run().await.unwrap();
+    });
 }
