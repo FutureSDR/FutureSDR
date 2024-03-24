2806f566242c51617cecd75ef067bb9ff1010b6e
Bastian Bloessl <mail@bastibl.net>
2024-03-24 15:18:23 +0100

    update wgpu perf wasm

---
+++ perf/wgpu/src/bin/perf.rs
@@ -0,0 +1,64 @@
+use futuresdr::anyhow::Result;
+
+#[cfg(not(target_arch = "wasm32"))]
+mod foo {
+    use clap::Parser;
+    use futuresdr::anyhow::Result;
+
+    #[derive(Parser, Debug)]
+    struct Args {
+        #[clap(short, long, default_value_t = 0)]
+        run: u64,
+        #[clap(short = 'S', long, default_value = "smol1")]
+        scheduler: String,
+        #[clap(short, long, default_value_t = 1000000)]
+        samples: u64,
+        #[clap(short, long, default_value_t = 4096)]
+        buffer_size: u64,
+    }
+
+    pub fn main() -> Result<()> {
+        let Args {
+            run,
+            scheduler,
+            samples,
+            buffer_size,
+        } = Args::parse();
+
+        futuresdr::async_io::block_on(perf_wgpu::run(run, scheduler, samples, buffer_size))?;
+        Ok(())
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+mod foo {
+    use futuresdr::anyhow::Result;
+    use leptos::*;
+
+    #[component]
+    /// Main GUI
+    fn Gui() -> impl IntoView {
+        let start = move |_| {
+            leptos::spawn_local(async {
+                perf_wgpu::run(0, "wasm".to_string(), 1000000, 32768)
+                    .await
+                    .unwrap();
+            });
+        };
+        view! {
+            <h1>"FutureSDR WGPU Perf"</h1>
+            <button on:click=start type="button" class="bg-fs-blue hover:brightness-75 text-slate-200 font-bold py-2 px-4 rounded">Start</button>
+        }
+    }
+
+    pub fn main() -> Result<()> {
+        _ = console_log::init_with_level(futuresdr::log::Level::Debug);
+        console_error_panic_hook::set_once();
+        mount_to_body(|| view! { <Gui /> });
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
+    foo::main()
+}
--- perf/wgpu/src/lib.rs
+++ perf/wgpu/src/lib.rs
@@ -1,12 +1,9 @@
 use std::iter::repeat_with;
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
 
 use futuresdr::anyhow::{Context, Result};
 use futuresdr::blocks::VectorSink;
 use futuresdr::blocks::VectorSource;
 use futuresdr::blocks::Wgpu;
-use futuresdr::log::info;
 use futuresdr::runtime::buffer::wgpu;
 use futuresdr::runtime::Flowgraph;
 use futuresdr::runtime::Runtime;
@@ -14,21 +11,7 @@ use futuresdr::runtime::Runtime;
 #[cfg(not(target_arch = "wasm32"))]
 use futuresdr::runtime::scheduler::SmolScheduler;
 
-#[cfg(target_arch = "wasm32")]
-use web_sys::console;
-
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen]
-pub async fn run_fg(run: u64, samples: u64, buffer_size: u64) {
-    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-    crate::run(run, "wasm".to_string(), samples, buffer_size)
-        .await
-        .unwrap();
-}
-
 pub async fn run(run: u64, scheduler: String, samples: u64, buffer_size: u64) -> Result<()> {
-    info!("start flowgraph");
-
     let orig: Vec<f32> = repeat_with(rand::random::<f32>)
         .take(samples as usize)
         .collect();
@@ -46,8 +29,6 @@ pub async fn run(run: u64, scheduler: String, samples: u64, buffer_size: u64) ->
 
     fg.connect_stream_with_type(src, "out", mul, "in", wgpu::H2D::new())?;
     fg.connect_stream_with_type(mul, "out", snk, "in", wgpu::D2H::new())?;
-
-    info!("start flowgraph");
 
     let runtime;
 
@@ -82,16 +63,13 @@ pub async fn run(run: u64, scheduler: String, samples: u64, buffer_size: u64) ->
 
     #[cfg(target_arch = "wasm32")]
     {
-        console::log_1(
-            &format!(
-                "{},{},{},{},{}",
-                run,
-                scheduler,
-                samples,
-                buffer_size,
-                elapsed.as_secs_f64()
-            )
-            .into(),
+        leptos::logging::log!(
+            "{},{},{},{},{}",
+            run,
+            scheduler,
+            samples,
+            buffer_size,
+            elapsed.as_secs_f64()
         );
     }
 
