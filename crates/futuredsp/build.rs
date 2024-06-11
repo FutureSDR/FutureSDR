736af9665c05c335b491173025198642cd3fd609
Bastian Bloessl <mail@bastibl.net>
2024-06-11 11:40:01 +0200

    futuredsp: disable cfg warning

--- crates/futuredsp/build.rs
+++ crates/futuredsp/build.rs
@@ -1,6 +1,7 @@
 use rustc_version::{version_meta, Channel};
 
 fn main() {
+    println!("cargo::rustc-check-cfg=cfg(RUSTC_IS_STABLE)");
     match version_meta().unwrap().channel {
         Channel::Stable => {
             println!("cargo:rustc-cfg=RUSTC_IS_STABLE");
