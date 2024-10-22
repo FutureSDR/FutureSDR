3bbf8b3c1fe0393d1a4d413b2b010edffd97980d
Simeon H.K. Fitch <simeon.fitch@datashapes.com>
2024-10-22 12:57:46 -0400

    Updated `message_handler` macro to emit fully qualified Result type in return types.

---
+++ tests/message_handler.rs
@@ -0,0 +1,43 @@
+/// This module exists to try to inhibit accidental import of [`futuresdr::anyhow::Result`]
+#[cfg(test)]
+mod isolated_scope {
+    /// Regression test for https://github.com/FutureSDR/FutureSDR/issues/149
+    #[test]
+    fn message_handler_compiles() {
+        use futuresdr::macros::message_handler;
+        use futuresdr::runtime::{Block, BlockMetaBuilder, BlockMeta, Kernel, MessageIo, MessageIoBuilder, Pmt, StreamIoBuilder, WorkIo};
+
+        struct MsgThing;
+
+        impl MsgThing {
+            fn new() -> Block {
+                Block::new(
+                    BlockMetaBuilder::new("MsgThing").build(),
+                    StreamIoBuilder::new()
+                        .build(),
+                    MessageIoBuilder::new()
+                        .add_input("in", Self::in_port)
+                        .build(),
+                    Self
+                )
+            }
+
+            #[message_handler]
+            async fn in_port(
+                &mut self,
+                _io: &mut WorkIo,
+                _mio: &mut MessageIo<Self>,
+                _meta: &mut BlockMeta,
+                _p: Pmt,
+            ) -> Result<Pmt> {
+                Ok(Pmt::Ok)
+            }
+        }
+
+        impl Kernel for MsgThing {}
+
+        // Main test is that the above compiles without futuresdr::anyhow::Result being imported in scope.
+        let b = MsgThing::new();
+        assert!(b.message_input_name_to_id("in").is_some());
+    }
+}
