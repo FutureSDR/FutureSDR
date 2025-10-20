20388d16b8d8654f98217351139d61adf51a0200
Bastian Bloessl <mail@bastibl.net>
2025-10-20 10:24:40 +0200

    Android more generic paths

--- examples/android/build.sh
+++ examples/android/build.sh
@@ -4,7 +4,7 @@ set -xe
 
 source ./env.sh
 
-rm -f ${PREFIX}/lib/libfuturesdr_android.so
-ln -s ${HOME}/.cargo/target/aarch64-linux-android/release/libfuturesdr_android.so ${PREFIX}/lib/
+rm -f ./FutureSDR/app/src/main/jni/arm64-v8a/libfuturesdr_android.so
+ln -s ${HOME}/.cargo/target/aarch64-linux-android/release/libfuturesdr_android.so ./FutureSDR/app/src/main/jni/arm64-v8a/
 
 cargo build --target aarch64-linux-android --lib --release
