e56e2a38664cac21c5bff454560b517d161f4673
Bastian Bloessl <mail@bastibl.net>
2025-10-20 11:48:46 +0200

    android: more robust build

--- examples/android/README.md
+++ examples/android/README.md
@@ -1,6 +1,18 @@
 # Android Example
 
+This example shows how to create an Android app that uses FutureSDR.
+It implements a simple FM receiver using the RTL-SDR.
+
 ## Getting Started
 
-- [Download](https://developer.android.com/studio) Android Studio
-- Open the FutureSDR Android project in this directory
+- Download and install [Android Studio](https://developer.android.com/studio)
+- Use tools -> SDK Manager to download the Android platform, NDK, and build tools. (This project uses Android 29 with NDK 28.2.13676358.)
+- Build the [FutureSDR Android toolchain](https://github.com/FutureSDR/android-sdr-toolchain)
+- Adapt the `jni` symbolic link (`./FutureSDR/app/src/main/jni`) in the Android app to your toolchain directory.
+- Adapt the Cargo config (`./.cargo/config.toml`) to point to your Android SDK.
+- Adapt the paths in the `build.sh` script to point to you Android SDK.
+- Run the `build.sh` script.
+- Open the FutureSDR Android project (`./FutureSDR/`) in Android Studio.
+- Build and install the app in Android Studio.
+- Close the app. Plugin the SDR. Start the app. You should be asked for permission to access the RTL-SDR and hear radio once it is granted.
+
--- examples/android/build.sh
+++ examples/android/build.sh
@@ -2,9 +2,31 @@
 
 set -xe
 
-source ./env.sh
+#############################################################
+### CONFIG
+#############################################################
+export TOOLCHAIN_ROOT=${HOME}/Android/Sdk/ndk/28.2.13676358
+export HOST_ARCH=linux-x86_64
+export PREFIX=${HOME}/src/android-sdr-toolchain/out/arm64-v8a
+export API_LEVEL=29
 
-rm -f ./FutureSDR/app/src/main/jni/arm64-v8a/libfuturesdr_android.so
-ln -s ${HOME}/.cargo/target/aarch64-linux-android/release/libfuturesdr_android.so ./FutureSDR/app/src/main/jni/arm64-v8a/
+#############################################################
+### DERIVED CONFIG
+#############################################################
+export SOAPYSDR_NO_PKG_CONFIG=1
+export SOAPY_SDR_ROOT=${PREFIX}
+export CMAKE_POLICY_VERSION_MINIMUM=3.5
 
+export TOOLCHAIN_BIN=${TOOLCHAIN_ROOT}/toolchains/llvm/prebuilt/${HOST_ARCH}/bin
+export CC="${TOOLCHAIN_BIN}/aarch64-linux-android${API_LEVEL}-clang"
+export CARGO_TARGET_AARCH64_LINUX_ANDROID_AR="${TOOLCHAIN_BIN}/llvm-ar"
+export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="${TOOLCHAIN_BIN}/aarch64-linux-android${API_LEVEL}-clang"
+
+#############################################################
+### BUILD
+#############################################################
 cargo build --target aarch64-linux-android --lib --release
+
+TARGET_DIR=$(cargo metadata --format-version=1 --no-deps | jq -r '.target_directory')
+cp ${TARGET_DIR}/aarch64-linux-android/release/libfuturesdr_android.so ./FutureSDR/app/src/main/jni/arm64-v8a/
+
