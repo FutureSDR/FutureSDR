675ae85ebcbdd194fb43292ad1cd99b6eddd0d0c
Bastian Bloessl <mail@bastibl.net>
2026-06-02 13:48:14 +0200

    runtime: reorganize logging

--- src/runtime/logging.rs
+++ src/runtime/logging.rs
@@ -7,6 +7,36 @@ pub fn init() {
     wasm::init();
 }
 
+#[cfg(all(not(target_os = "android"), not(target_arch = "wasm32")))]
+mod native {
+    use tracing_subscriber::filter::EnvFilter;
+    use tracing_subscriber::fmt;
+    use tracing_subscriber::prelude::*;
+
+    use crate::runtime::config;
+
+    pub(super) fn init() {
+        let format = fmt::layer()
+            .with_level(true)
+            .with_target(true)
+            .with_thread_ids(false)
+            .with_thread_names(true)
+            .compact();
+
+        let level = config::config().log_level;
+        let filter = EnvFilter::builder()
+            .with_default_directive(level.into())
+            .with_env_var("FUTURESDR_LOG")
+            .from_env_lossy();
+
+        let subscriber = tracing_subscriber::registry().with(filter).with(format);
+
+        if tracing::subscriber::set_global_default(subscriber).is_err() {
+            tracing::debug!("logger already initialized");
+        }
+    }
+}
+
 #[cfg(all(target_os = "android", not(target_arch = "wasm32")))]
 mod android {
     use once_cell::sync::OnceCell;
@@ -35,35 +65,5 @@ mod android {
 mod wasm {
     pub(super) fn init() {
         let _ = tracing_wasm::try_set_as_global_default();
-    }
-}
-
-#[cfg(all(not(target_os = "android"), not(target_arch = "wasm32")))]
-mod native {
-    use tracing_subscriber::filter::EnvFilter;
-    use tracing_subscriber::fmt;
-    use tracing_subscriber::prelude::*;
-
-    use crate::runtime::config;
-
-    pub(super) fn init() {
-        let format = fmt::layer()
-            .with_level(true)
-            .with_target(true)
-            .with_thread_ids(false)
-            .with_thread_names(true)
-            .compact();
-
-        let level = config::config().log_level;
-        let filter = EnvFilter::builder()
-            .with_default_directive(level.into())
-            .with_env_var("FUTURESDR_LOG")
-            .from_env_lossy();
-
-        let subscriber = tracing_subscriber::registry().with(filter).with(format);
-
-        if tracing::subscriber::set_global_default(subscriber).is_err() {
-            tracing::debug!("logger already initialized");
-        }
     }
 }
