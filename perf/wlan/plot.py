91f5347b3f90c1a200755b19e0951a10438801c0
Bastian Bloessl <mail@bastibl.net>
2026-06-09 15:02:21 +0200

    Remove WLAN opti perf config

--- perf/wlan/plot.py
+++ perf/wlan/plot.py
@@ -8,40 +8,26 @@ plt.style.use("../acmart.mplrc")
 
 d = pd.read_csv("perf-data/results.csv")
 g = (
-    d.groupby(["frames", "sdr", "config"], as_index=False)
+    d.groupby(["frames", "sdr"], as_index=False)
     .agg(time_mean=("time", "mean"))
-    .sort_values(["frames", "sdr", "config"])
+    .sort_values(["frames", "sdr"])
 )
 
-pivot = g.pivot(index="frames", columns=["sdr", "config"], values="time_mean").sort_index()
+pivot = g.pivot(index="frames", columns="sdr", values="time_mean").sort_index()
 print(pivot)
 
 frames = pivot.index.to_numpy()
 x = np.arange(len(frames))
-width = 0.24
+width = 0.36
 
-fs_normal = (
-    pivot[("fs", "normal")].to_numpy()
-    if ("fs", "normal") in pivot.columns
-    else np.zeros(len(frames))
-)
-fs_opti = (
-    pivot[("fs", "opti")].to_numpy()
-    if ("fs", "opti") in pivot.columns
-    else np.zeros(len(frames))
-)
-gr_legacy = (
-    pivot[("gr", "legacy")].to_numpy()
-    if ("gr", "legacy") in pivot.columns
-    else np.zeros(len(frames))
-)
+fs = pivot["fs"].to_numpy() if "fs" in pivot.columns else np.zeros(len(frames))
+gr = pivot["gr"].to_numpy() if "gr" in pivot.columns else np.zeros(len(frames))
 
 fig, ax = plt.subplots(1, 1)
 fig.subplots_adjust(bottom=0.2, left=0.12, top=0.98, right=0.98)
 
-ax.bar(x - width, fs_normal, width, label="FutureSDR normal")
-ax.bar(x, fs_opti, width, label="FutureSDR opti")
-ax.bar(x + width, gr_legacy, width, label="GNU Radio")
+ax.bar(x - width / 2, fs, width, label="FutureSDR")
+ax.bar(x + width / 2, gr, width, label="GNU Radio")
 
 ax.set_xticks(x)
 ax.set_xticklabels([str(v) for v in frames])
--- perf/wlan/rx.rs
+++ perf/wlan/rx.rs
@@ -1,15 +1,10 @@
 use clap::Parser;
-use clap::ValueEnum;
 use futuresdr::blocks::Apply;
 use futuresdr::blocks::Combine;
 use futuresdr::blocks::Delay;
 use futuresdr::blocks::Fft;
 use futuresdr::blocks::FileSource;
 use futuresdr::prelude::*;
-use futuresdr::runtime::buffer::circular;
-use perf::local_mpsc;
-use perf::local_spsc;
-use perf::local_spsc_tags;
 use std::time;
 
 use wlan::Decoder;
@@ -17,12 +12,6 @@ use wlan::FrameEqualizer;
 use wlan::MovingAverage;
 use wlan::SyncLong;
 use wlan::SyncShort;
-
-#[derive(Copy, Clone, Debug, ValueEnum)]
-enum Config {
-    Normal,
-    Opti,
-}
 
 #[derive(Parser, Debug)]
 #[clap(version)]
@@ -33,9 +22,6 @@ struct Args {
     /// File name
     #[clap(short, long, default_value = "wlan-100.cf32")]
     file: String,
-    /// Runtime config
-    #[clap(long, value_enum, default_value_t = Config::Normal)]
-    config: Config,
     /// FutureSDR buffer size in bytes
     #[clap(long, default_value_t = 262_144)]
     buffer_size: i64,
@@ -59,7 +45,7 @@ struct Args {
 //     Ok(out)
 // }
 
-fn normal(args: Args) -> Result<()> {
+fn run(args: Args) -> Result<()> {
     let mut fg = Flowgraph::new();
 
     let src = FileSource::<Complex32>::new(&args.file, false);
@@ -90,122 +76,7 @@ fn normal(args: Args) -> Result<()> {
     runtime.run(fg)?;
     let elapsed = now.elapsed();
 
-    println!(
-        "{},{},normal,{}",
-        args.run,
-        args.file,
-        elapsed.as_secs_f64()
-    );
-
-    Ok(())
-}
-
-fn local_domains(fg: &mut Flowgraph, n: usize) -> Result<Vec<LocalDomain>> {
-    let cores = core_affinity::get_core_ids().unwrap_or_default();
-    let mut domains = Vec::with_capacity(n);
-    if cores.len() >= n {
-        for core in cores.into_iter().take(n) {
-            domains.push(fg.local_domain_pinned(core.id)?);
-        }
-    } else {
-        for _ in 0..n {
-            domains.push(fg.local_domain()?);
-        }
-    }
-    Ok(domains)
-}
-
-fn opti(args: Args) -> Result<()> {
-    type CircularComplexReader = circular::Reader<Complex32>;
-    type CircularComplexWriter = circular::Writer<Complex32>;
-    type LocalMpscComplexReader = local_mpsc::Reader<Complex32>;
-    type LocalMpscComplexWriter = local_mpsc::Writer<Complex32>;
-    type LocalSpscComplexReader = local_spsc::Reader<Complex32>;
-    type LocalSpscComplexWriter = local_spsc::Writer<Complex32>;
-    type LocalSpscF32Reader = local_spsc::Reader<f32>;
-    type LocalSpscF32Writer = local_spsc::Writer<f32>;
-    type LocalSpscTagsComplexReader = local_spsc_tags::Reader<Complex32>;
-    type LocalSpscTagsComplexWriter = local_spsc_tags::Writer<Complex32>;
-    type LocalSpscTagsU8Reader = local_spsc_tags::Reader<u8>;
-    type LocalSpscTagsU8Writer = local_spsc_tags::Writer<u8>;
-
-    let mut fg = Flowgraph::new();
-
-    let domains = local_domains(&mut fg, 2)?;
-    let [local0, local1]: [LocalDomain; 2] = domains
-        .try_into()
-        .map_err(|_| anyhow::anyhow!("expected two local domains"))?;
-
-    let file = args.file.clone();
-    let src = fg.add_local(local0, move || {
-        FileSource::<Complex32, LocalMpscComplexWriter>::new(&file, false)
-    })?;
-    let delay = fg.add_local(local0, || {
-        Delay::<Complex32, LocalMpscComplexReader, LocalMpscComplexWriter>::new(16)
-    })?;
-    let complex_to_mag_2 = fg.add_local(local0, || {
-        Apply::<_, _, _, LocalMpscComplexReader, LocalSpscF32Writer>::with_buffers(
-            |i: &Complex32| i.norm_sqr(),
-        )
-    })?;
-    let float_avg = fg.add_local(local0, || {
-        MovingAverage::<f32, LocalSpscF32Reader, LocalSpscF32Writer>::new(64)
-    })?;
-    let mult_conj = fg.add_local(local0, || {
-        Combine::<
-            _,
-            _,
-            _,
-            _,
-            LocalMpscComplexReader,
-            LocalMpscComplexReader,
-            LocalSpscComplexWriter,
-        >::with_buffers(|a: &Complex32, b: &Complex32| a * b.conj())
-    })?;
-    let complex_avg = fg.add_local(local0, || {
-        MovingAverage::<Complex32, LocalSpscComplexReader, LocalMpscComplexWriter>::new(48)
-    })?;
-    let divide_mag = fg.add_local(local0, || {
-        Combine::<_, _, _, _, LocalMpscComplexReader, LocalSpscF32Reader, LocalSpscF32Writer>::with_buffers(
-            |a: &Complex32, b: &f32| a.norm() / b,
-        )
-    })?;
-    let sync_short = fg.add_local(local0, || {
-        SyncShort::<
-            LocalMpscComplexReader,
-            LocalMpscComplexReader,
-            LocalSpscF32Reader,
-            CircularComplexWriter,
-        >::new()
-    })?;
-
-    let sync_long = fg.add_local(local1, || {
-        SyncLong::<CircularComplexReader, LocalSpscTagsComplexWriter>::new()
-    })?;
-    let fft = fg.add_local(local1, || {
-        Fft::<LocalSpscTagsComplexReader, LocalSpscTagsComplexWriter>::with_buffers(64)
-    })?;
-    let frame_equalizer = fg.add_local(local1, || {
-        FrameEqualizer::<LocalSpscTagsComplexReader, LocalSpscTagsU8Writer>::new()
-    })?;
-    let decoder = fg.add_local(local1, || Decoder::<LocalSpscTagsU8Reader>::new())?;
-
-    connect!(fg, src ~> delay;
-        src ~> complex_to_mag_2 ~> float_avg;
-        src ~> in0.mult_conj ~> complex_avg;
-        delay ~> in_sig.sync_short;
-        complex_avg ~> in_abs.sync_short;
-        divide_mag ~> in_cor.sync_short;
-        delay ~> in1.mult_conj;
-        complex_avg ~> in0.divide_mag; float_avg ~> in1.divide_mag;
-        sync_short > sync_long ~> fft ~> frame_equalizer ~> decoder);
-
-    let runtime = Runtime::new();
-    let now = time::Instant::now();
-    runtime.run(fg)?;
-    let elapsed = now.elapsed();
-
-    println!("{},{},opti,{}", args.run, args.file, elapsed.as_secs_f64());
+    println!("{},{},{}", args.run, args.file, elapsed.as_secs_f64());
 
     Ok(())
 }
@@ -214,11 +85,7 @@ fn main() -> Result<()> {
     let args = Args::parse();
     futuresdr::runtime::config::set("ctrlport_enable", false);
     futuresdr::runtime::config::set("buffer_size", args.buffer_size);
-
-    match args.config {
-        Config::Normal => normal(args)?,
-        Config::Opti => opti(args)?,
-    }
+    run(args)?;
 
     Ok(())
 }
