c086e35596ad8cb62fcd7a5c7c830fbc0bbb499e
Bastian Bloessl <mail@bastibl.net>
2026-07-07 09:43:05 +0200

    perf: consolidate benchmark setup scripts

--- perf/perf_setup.sh
+++ perf/perf_setup.sh
@@ -5,6 +5,14 @@ set -xe
 SYSTEM_CPUS=0,1,6,7
 SDR_CPUS=2,3,4,5
 
+echo "==> Setting CPU governor to performance"
+for cpu in /sys/devices/system/cpu/cpu[0-9]*; do
+  governor="$cpu/cpufreq/scaling_governor"
+  if [[ -e "$governor" ]]; then
+    echo performance | sudo tee "$governor" > /dev/null
+  fi
+done
+
 echo "==> Setting AllowedCPUs to: ${SYSTEM_CPUS}"
 sudo systemctl set-property --runtime sdr.slice AllowedCPUs=${SDR_CPUS}
 sudo systemctl set-property --runtime user.slice AllowedCPUs=${SYSTEM_CPUS}
--- perf/perf_teardown.sh
+++ perf/perf_teardown.sh
@@ -12,3 +12,11 @@ sudo systemctl set-property --runtime init.scope   AllowedCPUs=$ALL_CPUS
 
 sudo systemctl stop sdr.slice || true
 sudo systemctl reset-failed sdr.slice || true
+
+echo "==> Setting CPU governor to powersave"
+for cpu in /sys/devices/system/cpu/cpu[0-9]*; do
+  governor="$cpu/cpufreq/scaling_governor"
+  if [[ -e "$governor" ]]; then
+    echo powersave | sudo tee "$governor" > /dev/null
+  fi
+done
