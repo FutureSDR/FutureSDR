d8c869b2cf7e94dd17f519f41d7e77639932ee3b
Bastian Bloessl <mail@bastibl.net>
2026-07-29 16:39:31 +0200

    Run buffer compile tests together

--- tests/buffer_compile_errors.rs
+++ tests/buffer_compile_errors.rs
@@ -1,11 +1,6 @@
 #[test]
-fn local_only_buffer_is_rejected_by_cross_domain_stream_api() {
+fn buffer_compile_errors() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/compile_fail/local_buffer_cross_domain.rs");
-}
-
-#[test]
-fn zero_sized_samples_are_rejected() {
-    let t = trybuild::TestCases::new();
     t.compile_fail("tests/compile_fail/zero_sized_sample.rs");
 }
